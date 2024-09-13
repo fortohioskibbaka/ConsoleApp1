70970167936d71339b723f4928e63809331affee
Parker Hammond <339720419@GoTVDSB.ca>
2024-09-13 09:25:05 -0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello, World!");
+            int age; 
+            Console.WriteLine("Enter your age:");
+            age = Console.Read();
+            Console.WriteLine("Your age is: " + age);
+            Console.ReadLine();
+        }
+    }
+}
