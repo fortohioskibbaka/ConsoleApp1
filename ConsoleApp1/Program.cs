ce358c3f9688fe50c78e2d07df912c8e472a3392
Parker Hammond <339720419@GoTVDSB.ca>
2024-09-20 09:22:18 -0400

    csd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,66 @@
-﻿namespace ConsoleApp1
+﻿internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-           
-            Console.WriteLine("Hello, World!");
-            int age; 
-            Console.WriteLine("Enter your age:");
-            age = Console.Read();
-            Console.WriteLine("Your age is: " + age);
-            Console.ReadLine();
-        }
+        string name;
+        int age;
+        string price;
+        string cart;
+        string lastname;
+        string firstname;
+        int avrage;
+        string login;
+        int grade;
+        int studentID;
+        string Name;
+        int grampsage;
+        
+       
+
+
+        Console.WriteLine("what is your name:");
+        name = Console.ReadLine();
+        Console.WriteLine("hi  " + name + " how old are u");
+        int.TryParse(Console.ReadLine(), out age);
+        Console.WriteLine("your only realy " + age);
+        Console.WriteLine("how much do u make parker?");
+        price = Console.ReadLine();
+        Console.WriteLine("wow you are super rich makeing " + price);
+        Console.ReadLine();
+        Console.WriteLine("plese enter you info below thank you");
+        Console.WriteLine("frist name");
+        firstname = Console.ReadLine();
+        Console.WriteLine("last name");
+        lastname = Console.ReadLine();
+        Console.WriteLine("what is your  avrage");
+        int.TryParse(Console.ReadLine(), out avrage);
+        Console.WriteLine("login");
+        login = Console.ReadLine();
+        Console.WriteLine("grade");
+        Int32.TryParse(Console.ReadLine(), out grade);
+        Console.WriteLine("student id");
+        int.TryParse(Console.ReadLine(),out studentID);
+
+        Console.WriteLine("firstname "+ firstname);
+        Console.WriteLine("lastname " + lastname);
+        Console.WriteLine("login " + login);
+        Console.WriteLine("grade " + grade);
+        Console.WriteLine("student ID " + studentID);    
+        Console.WriteLine("avrage " + avrage);
+
+        Console.WriteLine("hello whats your name");
+        Name = Console.ReadLine();
+        Console.WriteLine("why nice to meet u " + Name + " how old are you");
+        int.TryParse (Console.ReadLine(), out grampsage);
+        Console.WriteLine("did you know in 5 years you will be " + grampsage + 5);
+
+
+
+
+
+        
+        
+        
+      
     }
 }
